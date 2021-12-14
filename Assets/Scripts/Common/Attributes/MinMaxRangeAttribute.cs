aaec4f403969984c4af44c1c5a0009d60611122d
TaiVan <vcmtspkt@gmail.com>
2021-12-14 14:56:33 +0700

    Added MinMaxRangeAttribute

---
+++ Assets/Scripts/Common/Attributes/MinMaxRangeAttribute.cs
@@ -0,0 +1,14 @@
+using System;
+namespace DP2D
+{
+    public class MinMaxRangeAttribute : Attribute
+    {
+        public float Min { get; private set; }
+        public float Max { get; private set; }
+        public MinMaxRangeAttribute(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
---
+++ Assets/Scripts/Common/RangeFloat.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace DP2D
+{
+    [Serializable]
+    public struct RangeFloat
+    {
+        public float minValue;
+        public float maxValue;
+    }
+}
